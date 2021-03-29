a34be50e0b2465ccd50ab9918c92db719cff442b
Ricardo Avancini <ravancini@PWST215s-MacBook-Pro.local>
2021-03-29 14:51:32 +0100

    Initial commit from Create Next App

---
+++ src/components/Main/index.tsx
@@ -0,0 +1,15 @@
+import * as S from './styles'
+
+const Main = () => (
+  <S.Wrapper>
+    <S.Logo src="/img/logo.svg" />
+    <S.Title>Título do Projeto</S.Title>
+    <S.Description>
+      Texto de descrição do projeto a ser implementado em TypeScript, ReactJS,
+      NextJS e Styled Components
+    </S.Description>
+    <S.Illustration src="/img/illustration.svg" />
+  </S.Wrapper>
+)
+
+export default Main
