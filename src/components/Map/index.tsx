f164c8280d9fd906c72f125b104c5d682bbfffe8
Ricardo Avancini <ravancini@PWST215s-MacBook-Pro.local>
2021-04-27 12:31:51 +0100

    Add google analytics

--- src/components/Map/index.tsx
+++ src/components/Map/index.tsx
@@ -1,6 +1,8 @@
-import { MapContainer, TileLayer, GeoJSON } from 'react-leaflet'
+import { MapContainer, TileLayer, GeoJSON, MapConsumer } from 'react-leaflet'
 import { Feature, GeoJsonObject } from 'geojson'
 import { Layer } from 'leaflet'
+
+import * as S from './styles'
 
 type Geometry = {
   id: string
@@ -23,38 +25,53 @@ const onEachFeature = (feature: Feature, layer: Layer) => {
 }
 
 const Map = ({ geometries }: MapProps) => (
-  <MapContainer
-    center={[-22.595447151697968, -43.200456906554251]}
-    zoom={16}
-    style={{ height: '100%', width: '100%' }}
-  >
-    <TileLayer
-      attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-      url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-    />
-    {/* <Marker position={[0, 0]} title="Point 01">
+  <S.MapWrapper>
+    <MapContainer
+      center={[-22.595447151697968, -43.200456906554251]}
+      zoom={16}
+      minZoom={10}
+      style={{ height: '100%', width: '100%' }}
+    >
+      <MapConsumer>
+        {(map) => {
+          const width =
+            window.innerWidth ||
+            document.documentElement.clientWidth ||
+            document.body.clientWidth
+          if (width < 768) {
+            map.setMinZoom(8)
+          }
+          return null
+        }}
+      </MapConsumer>
+      <TileLayer
+        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      {/* <Marker position={[0, 0]} title="Point 01">
       <Popup>
         A pretty CSS3 popup. <br /> Easily customizable.
       </Popup>
     </Marker> */}
 
-    {geometries?.map((geo: Geometry) => {
-      return (
-        <GeoJSON
-          key={geo.slug}
-          attribution="credits due..."
-          data={geo.geojson}
-          onEachFeature={onEachFeature}
-          /* eventHandlers={{
+      {geometries?.map((geo: Geometry) => {
+        return (
+          <GeoJSON
+            key={geo.slug}
+            attribution="credits due..."
+            data={geo.geojson}
+            onEachFeature={onEachFeature}
+            /* eventHandlers={{
             click: (e) => {
               console.log(e.layer.feature.properties.title)
               console.log(e)
             }
           }} */
-        ></GeoJSON>
-      )
-    })}
-  </MapContainer>
+          ></GeoJSON>
+        )
+      })}
+    </MapContainer>
+  </S.MapWrapper>
 )
 
 export default Map
